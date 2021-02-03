4b4d1abc655ec1e8397f38ed3eb1206b4c099c16
Mateo Loaiza Rios <mateoloaiza1227@gmail.com>
2021-02-03 18:04:54 -0500

    finished v1

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Heroe } from '../interfaces/heroes.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]>{
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+  }
+
+  getHeroeById(id: string): Observable<Heroe>{
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+  }
+
+  getSugerencias(termino: string): Observable<Heroe[]>{
+    return this.http.get<Heroe[]>(`http://localhost:3000/heroes?q=${termino}&_limit=6`);
+  }
+}
